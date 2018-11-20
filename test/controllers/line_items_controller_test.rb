c952027c179f5bfb3aa07d596138888f7d005942
David Ruiz de la Rosa <ddlarosa@thesensorylab.es>
2018-11-20 17:12:02 +0100

    Add Ajax To Store

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -17,12 +17,14 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post line_items_url, params: { product_id: @line_item.product_id }
+      post line_items_url, params: { product_id: @line_item.product_id }, xhr: true
     end
 
-    follow_redirect!
-    assert_select 'h2', "Your Cart"
-    assert_select 'td', "Programming Ruby 1.9"
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
+    end
+    
   end
 
   test "should show line_item" do
